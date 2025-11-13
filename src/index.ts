0f38f227098cf8cb63a00f604fe6832eee6e81f8
konstantin.filimonov <konstantinfilimonou@gmail.com>
2025-11-14 00:37:15 +0300

    Tested commit

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,6 @@ setupApp(app);
 // порт приложения
 const PORT = process.env.PORT || 5001;
 
-// запуск приложения
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`);
 });
